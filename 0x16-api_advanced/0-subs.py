956d5176172a02e53dea16603395d59c0eca9cd4
Otmane TOUHAMI <Otmanetouhami01@gmail.com>
2024-06-04 12:15:18 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
+'''A module containing functions for working with the Reddit API.
+'''
 import requests
 
 
